e34404d0d5c5aa2b3f0fa2cf536b2fcf69d896ce
Alex Akayomov <a.akayomov@gmail.com>
2020-07-04 15:11:09 +0300

    added python entrypoint

--- entrypoint.py
+++ entrypoint.py
@@ -1,5 +1,37 @@
+import os
 import subprocess
+from argparse import ArgumentParser
 
-unit = str(subprocess.run(['uname', '-n'], stdout=subprocess.PIPE).stdout, 'utf-8').rstrip()
 
-print("Running on node: "+unit)
+class EntryPoint:
+    def __init__(self):
+        unit = str(subprocess.run(['uname', '-n'], stdout=subprocess.PIPE).stdout, 'utf-8').rstrip()
+        print("Running on node: " + unit)
+
+        self.path = str(subprocess.run(['pwd'], stdout=subprocess.PIPE).stdout, 'utf-8').rstrip()
+
+        self.argp = ArgumentParser(description="Automation entry point")
+        self.argp.add_argument('-p', '--pid', help="PID file path")
+        self.argv = self.argp.parse_args()
+
+        selector = {
+            'nibiru': self.keenetic_router,
+            'oakaiomov-devtool': self.keenetic_router,
+        }
+
+        selector.get(unit, self.unknown)()
+
+    def keenetic_router(self):
+        runner = self.path+"/routers/keenetic/runner.py"
+        process = subprocess.Popen(['python3', runner], cwd=self.path, stdout=subprocess.PIPE)
+        print("KeeneticRunner: service started with pid '"+str(process.pid)+"'")
+        if self.argv.pid:
+            open(self.argv.pid, 'w').write(str(process.pid))
+
+    @staticmethod
+    def unknown():
+        print("Started on unknown node")
+
+
+if __name__ == "__main__":
+    EntryPoint()
---
+++ routers/keenetic/runner.py
@@ -0,0 +1,13 @@
+import time
+
+
+class KeeneticRunner:
+    def __init__(self):
+        print("Initialized KeeneticRunner")
+        while True:
+            print("I'm online")
+            time.sleep(5)
+
+
+if __name__ == "__main__":
+    KeeneticRunner()
