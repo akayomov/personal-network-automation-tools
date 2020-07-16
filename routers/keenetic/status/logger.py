7743d51bbb6d1e021cdc29f3fdbaba1c40f1e937
Alex Akayomov <a.akayomov@gmail.com>
2020-07-17 00:26:04 +0300

    handling ioerror for print

--- routers/keenetic/status/logger.py
+++ routers/keenetic/status/logger.py
@@ -5,13 +5,19 @@ import traceback
 def log(*args):
     message = " ".join(map(lambda x: str(x), args))
     os.system("logger -p 6 -t automation \""+message+"\"")
-    print('\x1b[1;36m' + message + '\x1b[0m')
+    try:
+        print('\x1b[1;36m' + message + '\x1b[0m')
+    except IOError:
+        os.system("logger -p 4 -t automation \"Broken pipe in log...\"")
 
 
 def warn(*args):
     message = " ".join(map(lambda x: str(x), args))
     os.system("logger -p 4 -t automation \""+message+"\"")
-    print('\x1b[1;33m' + message + '\x1b[0m')
+    try:
+        print('\x1b[1;33m' + message + '\x1b[0m')
+    except IOError:
+        os.system("logger -p 4 -t automation \"Broken pipe in warn...\"")
 
 
 def error(e: Exception):
@@ -22,5 +28,8 @@ def error(e: Exception):
     trace = "".join(traceback.format_exc())
     os.system("logger -p 3 -t automation \""+message+"\"")
     os.system("logger -p 3 -t automation \""+trace+"\"")
-    print('\x1b[1;31m' + message + '\x1b[0m')
-    print('\x1b[31m' + trace + '\x1b[0m\n')
+    try:
+        print('\x1b[1;31m' + message + '\x1b[0m')
+        print('\x1b[31m' + trace + '\x1b[0m\n')
+    except IOError:
+        os.system("logger -p 4 -t automation \"Broken pipe in error...\"")
