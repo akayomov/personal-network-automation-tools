254895e74fdd1e34b829b77eaa292c92eaa3eb5b
Alex Akayomov <a.akayomov@gmail.com>
2020-07-16 23:56:41 +0300

    logging traceback

--- routers/keenetic/status/logger.py
+++ routers/keenetic/status/logger.py
@@ -19,6 +19,8 @@ def error(e: Exception):
         message = "Exception {}: <{}>".format(str(e.errno), e.strerror)
     except AttributeError:
         message = "Exception: <{}>".format(str(e))
+    trace = "".join(traceback.format_exc())
     os.system("logger -p 3 -t automation \""+message+"\"")
+    os.system("logger -p 3 -t automation \""+trace+"\"")
     print('\x1b[1;31m' + message + '\x1b[0m')
-    print('\x1b[31m' + "".join(traceback.format_exc()) + '\x1b[0m\n')
+    print('\x1b[31m' + trace + '\x1b[0m\n')
